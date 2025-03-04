a85e0012e4664a887e3e0b0d284b37f803e71118
Davenads <davidnadler9637@gmail.com>
2025-03-04 15:56:31 -0500

    Changing the google key refs for challenge cmd too for further troubleshooting

--- commands/challenge.js
+++ commands/challenge.js
@@ -1,18 +1,10 @@
 require('dotenv').config()
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
-const { google } = require('googleapis')
 const { logError } = require('../logger')
 const redisClient = require('../redis-client');
+const sheets = require('../google-sheets-client');
 
-const sheets = google.sheets({
-  version: 'v4',
-  auth: new google.auth.JWT(
-    process.env.GOOGLE_CLIENT_EMAIL,
-    null,
-    process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    ['https://www.googleapis.com/auth/spreadsheets']
-  )
-});
+
 
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID
 const SHEET_NAME = 'SvS Ladder'
